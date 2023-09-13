729d0e300ec21f77bc4edef5f181e68c7b29e038
MelvilPRO <melvil.bouron@stagiairesmns.fr>
2023-09-13 15:58:47 +0200

    Partie du cours sans code html

--- index.php
+++ index.php
@@ -1,17 +1,51 @@
-<h1>
-    <?php echo "Hello World! PHP!"; ?>
-    <?php 
-        echo "<em>Texte en italique avec partie HTML insérée dans le code PHP</em>";
-    ?>
-    <p>
-        <?php
-            echo "Lorem ipsum...";
-        ?>
-    </p>
-    <?php
-        for ($i = 1; $i <= 20; $i++) {
-            echo "<br>";
-            echo $i;
-        }  
-    ?>
-</h1>
+<?php
+    // Les définitions des variables...
+    $variable = "Hello World!";
+    $floatVar = 9.0;
+
+    // Les boucles...
+    for ($index = 0; $index < 20; $index++)
+    {
+        echo $variable;
+        echo "<br>";
+    }
+
+    // Les conditions...
+    if ($floatVar <= 10)
+    {
+        echo "floatVar est inférieur ou égal a 10";
+    }
+
+    // Les fonctions...
+    function similaireAJavaScript($param1, $param2)
+    {
+        echo "<br>";
+        echo $param1;
+        echo "<br>";
+        echo $param2;
+        echo "<br>";
+    }
+    similaireAJavaScript("Coucou", "Thomas");
+
+    // Les tableaux...
+    $tabNumbers = [1, 2, 3, 0xFF];
+    echo $tabNumbers[3];
+    echo "<br>";
+
+    $tabArchive = [
+        "Première case" => 42, 
+        "Deuxième case" => "Incroyable"
+    ];
+    echo $tabArchive["Deuxième case"];
+
+    // Les boucles foreach...
+    // foreach ($tabArchive as $value) indexée
+    foreach ($tabArchive as $key => $value)
+    {
+        echo "<br/>";
+        echo $key;
+        echo "<br/>";
+        echo $value;
+        echo "<br/>";
+    }
+?>
